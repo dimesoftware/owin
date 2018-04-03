fe3ac6832e624d56636fa00241d95032be52f119
Hendrik Bulens <hendrik.bulens@gmail.com>
2018-04-03 16:13:13 +0100

    Migrate from TFVC

---
+++ src/core/IOwinContextUtilities.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Microsoft.Owin
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public static class OwinContextUtilities
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public static bool SupportsEncoding(this IOwinContext context, string encoding)
+        {
+            IEnumerable<string> acceptedEncoding = context.Request.Headers["Accept-Encoding"]?.Split(',');
+            return acceptedEncoding.Any(x => string.Equals(x, encoding, StringComparison.OrdinalIgnoreCase));
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public static bool IsAjaxRequest(this IOwinContext context)
+            => context.Request.Headers.ContainsKey("X-Requested-With")
+               && context.Request.Headers["X-Requested-With"] == "XMLHttpRequest";
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public static bool IsWebSocketRequest(this IOwinContext context)
+            => context.Request.Path.ToString().Contains("signalr");
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public static bool IsFileRequest(this IOwinContext context)
+        {
+            string requestUriExtension = Path.GetExtension(context.Request.Path.ToString());
+            return !string.IsNullOrEmpty(requestUriExtension);
+        }
+    }
+}
---
+++ src/core/IOwinRequestUtilities.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace Microsoft.Owin
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public static class OwinRequestUtilities
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>       
+        public static bool IsBearerTokenRequest(this IOwinRequest request)
+        {
+            Microsoft.Owin.IHeaderDictionary headers = request.Headers;
+            string authorizationHeader = headers["Authorization"] ?? string.Empty;
+
+            return CultureInfo.InvariantCulture.CompareInfo.IndexOf(authorizationHeader, "Bearer", CompareOptions.IgnoreCase) >= 0; ;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>    
+        public static bool IsSuppressedRequest(this IOwinRequest request)
+        {
+            Microsoft.Owin.IHeaderDictionary responseHeader = request.Headers;
+            if (responseHeader == null)
+                return false;
+
+            return (responseHeader["Suppress-Redirect"] == "True");
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>       
+        public static bool IsAjaxRequest(this IOwinRequest request)
+        {
+            string xmlHttpRequest = "XMLHttpRequest";
+
+            IReadableStringCollection query = request.Query;
+            Microsoft.Owin.IHeaderDictionary headers = request.Headers;
+
+            string requestedWithQuery = query["X-Requested-With"] ?? string.Empty;
+            string requestedWithHeaders = headers["X-Requested-With"] ?? string.Empty;
+
+            if (query != null && requestedWithQuery.Equals(xmlHttpRequest, StringComparison.InvariantCulture))
+                return true;
+
+            return headers != null&& requestedWithHeaders.Equals(xmlHttpRequest, StringComparison.InvariantCulture);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        internal static bool IsWebSocketRequest(this IOwinRequest context)
+        {
+            return context.Path.ToString().Contains("signalr");
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        internal static bool IsFileRequest(this IOwinRequest context)
+        {
+            string requestUriExtension = Path.GetExtension(context.Path.ToString());
+            return !string.IsNullOrEmpty(requestUriExtension);
+        }
+    }
+}
